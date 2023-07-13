44a13d96ddfb221264a4d03870cc7b60a94432ec
Фёдор <fedor@MacBook-Air-Fedor.local>
2023-07-14 00:29:47 +0500

    Initial commit

---
+++ game_engine.py
@@ -0,0 +1,52 @@
+from game_settings import max_len_words as maxlw, min_len_words as minlw, gallows_states
+from random import randint
+DICT = [x.strip() for x in open('source/russian_nouns.txt').readlines() if minlw <= len(x.strip()) <= maxlw]
+
+
+def createword():
+    word = DICT[randint(0, len(DICT)-1)]
+    return word, len(word) * '*', len(word)
+
+
+def open_letters(word, set_with_chars):
+    hidden_word = ''
+    for i in word:
+        if i in set_with_chars:
+            hidden_word += i
+        else:
+            hidden_word += '*'
+    return hidden_word
+
+
+def mistake(count_unknown_letters, hidden_word):
+    if count_unknown_letters == hidden_word.count('*'):
+        print('You made a mistake :()')
+        return True
+    else:
+        return False
+
+
+def draw(number_of_mistakes):
+    for i in gallows_states[number_of_mistakes]:
+        print(i)
+
+
+def game_end(count_mistakes):
+    if count_mistakes == 6:
+        print('You lose -_-')
+    return not(count_mistakes == 6)
+
+
+def input_validity(input_str):
+    if input_str in ('Y', 'N'):
+        return input_str
+    else:
+        raise Exception("Ошибка ввода, попробуйте ввести 'Y' или 'N'")
+
+
+def char_is_validity(input_str):
+    rus_alphabet = ''.join([chr(i) for i in range(ord('а'), ord('я') + 1)]) + 'ё'
+    if len(input_str) == 1 and input_str in rus_alphabet:
+        return input_str
+    else:
+        raise Exception("Ошибка ввода, попробуйте ввести одну строчную букву русского алфавита")
---
+++ play_game.py
@@ -0,0 +1,23 @@
+from game_engine import *
+
+game_is_run = 0
+while True:
+    if not game_is_run:
+        print('Do you like to play? [Y]es or [N]o')
+        if input_validity(input()) == 'Y':
+            game_is_run = 1
+            set_with_char = set()
+            word, hidden_word, count_unknown_letters = createword()
+            count_mistakes = 0
+            draw(count_mistakes)
+        else:
+            print('Ok bay-bay, see you later!')
+            break
+
+    print(f'You word is {hidden_word}, number of mistakes: {count_mistakes}\nChose you char:')
+    set_with_char.add(char_is_validity(input()))
+    hidden_word = open_letters(word, set_with_char)
+    count_mistakes += mistake(count_unknown_letters, hidden_word)
+    count_unknown_letters = hidden_word.count('*')
+    draw(count_mistakes)
+    game_is_run = game_end(count_mistakes)
